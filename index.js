e0b20d333832b0a778ae501f2b8eba00c24b0637
Dario Walter Carrizo <108595018+dwc1970@users.noreply.github.com>
2024-02-28 17:27:40 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Obtener el elemento de video
+  const video = document.getElementById("C:\Users\eliza\Desktop\video_player-main\images\videos");
+
+  // Reproducir el video automáticamente
+  video.play();
+});

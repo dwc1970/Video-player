7ed96e513c7170c1f2a7561043d9b84b37ad4638
Dario Walter Carrizo <108595018+dwc1970@users.noreply.github.com>
2024-09-28 17:03:37 -0300

    Update index.js
    actualizacion

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ document.addEventListener("DOMContentLoaded", function () {
   // Obtener el elemento de video
   const video = document.getElementById("C:\Users\eliza\Desktop\video_player-main\images\videos");
 
-  // Reproducir el video automáticamente
+  // Reproducir el video automaticamente 
   video.play();
 });
